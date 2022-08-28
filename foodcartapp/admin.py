1a206e07d9a12b07709aea17f7902ff5adea9b5d
tbaiguzhinov <tim.baiguzhinov@gmail.com>
2022-08-28 23:23:03 +0600

    Change super in admin

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -126,7 +126,7 @@ class OrderAdmin(admin.ModelAdmin):
     ]
 
     def response_change(self, request, obj):
-        res = super(OrderAdmin, self).response_change(request, obj)
+        res = super().response_change(request, obj)
         if "next" in request.GET and url_has_allowed_host_and_scheme(
             request.GET['next'],
             allowed_hosts=None,
